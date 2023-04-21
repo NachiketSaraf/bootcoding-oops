0d981b8ab4e0e1407d03eef709e13abd5ed15ecf
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-21 23:27:25 +0530

    hospital application

---
+++ src/main/java/com/bootcoding/oops/HospitalApp.java
@@ -0,0 +1,35 @@
+package com.bootcoding.oops;
+
+import com.bootcoding.oops.encapsulation.hospital.Hospital;
+import com.bootcoding.oops.encapsulation.patient.Patient;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+public class HospitalApp {
+    public static void main(String[] args) {
+        Patient ram = new Patient();
+        ram.setName("Ram");
+        ram.setDateOfAdmission(getPreviousDate(4));
+        ram.setDateOfDischarged(new Date());
+        ram.setDisease("Cold");
+        System.out.println(ram);
+
+        Hospital orangeCityHospital = new Hospital();
+        orangeCityHospital.setName("Orange City Hospital");
+        orangeCityHospital.setAddress("Orange city square Nagpur");
+        orangeCityHospital.setServices("Multi-specialist Hospital");
+        orangeCityHospital.setId(UUID.randomUUID());
+
+        System.out.println(orangeCityHospital);
+    }
+    private static Date getPreviousDate(int days){
+        Calendar cal =Calendar.getInstance();
+        cal.add(Calendar.DATE,days*-1);
+        //System.out.println("Date = "+ cal.getTime());
+        return cal.getTime();
+
+    }
+
+}

c9709ef04eb7ee73cde3b375327067adefeb6f46
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-26 20:41:16 +0530

    Hospital

--- src/main/java/com/bootcoding/oops/encapsulation/hospital/Hospital.java
+++ src/main/java/com/bootcoding/oops/encapsulation/hospital/Hospital.java
@@ -24,10 +24,6 @@ public class Hospital {
         this.address = address;
     }
 
-    public String getServices() {
-        return services;
-    }
-
     public void setServices(String services) {
         this.services = services;
     }

d4dd19c760cdbb2e8f853f9a4af684abe4504e88
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-29 19:47:37 +0530

    HospitalApp

--- src/main/java/com/bootcoding/oops/encapculation_app/HospitalApp.java
+++ src/main/java/com/bootcoding/oops/encapculation_app/HospitalApp.java
@@ -1,4 +1,4 @@
-package com.bootcoding.oops;
+package com.bootcoding.oops.encapculation_app;
 
 import com.bootcoding.oops.encapsulation.hospital.Hospital;
 import com.bootcoding.oops.encapsulation.patient.Patient;

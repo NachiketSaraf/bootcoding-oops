b706b8318a085c71feec2e3d25fdf74e11a8adae
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-29 20:39:48 +0530

    Banner class with its property

--- src/main/java/com/bootcoding/oops/inheritance/banner/Banner.java
+++ src/main/java/com/bootcoding/oops/inheritance/banner/Banner.java
@@ -9,7 +9,7 @@ public class Banner {
     String dimension;
     double price;
     Banner(){
-        System.out.println("Banner has been created");
+        System.out.println("Banner object is created");
     }
     public void create(){
         System.out.println("Banner has been created");

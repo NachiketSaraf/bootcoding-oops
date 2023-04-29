247e1fa2234f5849a148a69cb6e7a23d662cfbd5
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-29 19:48:14 +0530

    Banner class with its property

---
+++ src/main/java/com/bootcoding/oops/inheritance/banner/Banner.java
@@ -0,0 +1,18 @@
+package com.bootcoding.oops.inheritance.banner;
+
+public class Banner {
+    String color;
+    String matter;
+    String type;
+    String material;
+    String text;
+    String dimension;
+    double price;
+
+    public void create(){
+        System.out.println("Banner has been created");
+    }
+    public void display(){
+        System.out.println("Display");
+    }
+}

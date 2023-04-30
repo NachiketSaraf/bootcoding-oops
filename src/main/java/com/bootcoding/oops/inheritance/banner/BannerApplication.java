ac99277804c1e95476075c9bad0ff829c1c7314a
NachiketSaraf <sarafnachiket@gmail.com>
2023-04-30 21:49:33 +0530

    Banner Application to create objects

--- src/main/java/com/bootcoding/oops/inheritance/banner/BannerApplication.java
+++ src/main/java/com/bootcoding/oops/inheritance/banner/BannerApplication.java
@@ -2,6 +2,12 @@ package com.bootcoding.oops.inheritance.banner;
 
 public class BannerApplication {
     public static void main(String[] args) {
+        Banner bn = new Banner();
+        bn.color = "white";
+        bn.material = "Hello Java";
+
+        bn.create();
+        bn.display();
         FlexBanner fb = new FlexBanner();
         fb.material = "New batches of java is starting soon";
         fb.type = "Flex banner";
